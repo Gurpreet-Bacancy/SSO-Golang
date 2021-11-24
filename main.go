c256e480da9e3a72162ded2404394bd7ddf6ccf8
Gurpreet singh guller <gurpreet.guller@neosoftmail.com>
2021-11-24 15:08:54 +0530

    SSO-Golang

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"golang-sso/controllers"
+	"net/http"
+)
+
+
+
+func main(){
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/",fs)
+	http.HandleFunc("/signin",controllers.Signin)
+	http.HandleFunc("/callback",controllers.Callback)
+	http.ListenAndServe(":3000",nil)
+}
